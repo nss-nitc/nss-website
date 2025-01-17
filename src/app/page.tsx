8460ef70c768392760eed38014028ebb0b0d4de4
Nishanth <nishanthrn1@gmail.com>
2025-01-17 23:34:37 +0530

    fix:made Hero better

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,3 @@
-import Events from "@/components/Events/Events";
-import Hero from "@/components/Hero/Hero";
-
 export default function page() {
-  return (
-    <>
-      <Hero />
-    </>
-  );
+  return <></>;
 }

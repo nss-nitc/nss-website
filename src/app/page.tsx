50dcf39af9a817478f8585bcdaeb7ee01d49e26c
Shazil Mohammmed K P <shazilmohemmedkp@gmail.com>
2025-01-18 02:29:18 +0530

    gallery done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,6 @@
-<<<<<<< HEAD
-import Gallery from "@/app/Gallery"
-
-export default function page() {
-  return (
-    <>
-      <Gallery></Gallery>
-    </>
-  );
-=======
 import Events from "@/components/Events/Events";
 import Hero from "@/components/Hero/Hero";
 
 export default function page() {
   return <></>;
->>>>>>> 7bd84d7592200970183e83631f40e9662804a32f
 }

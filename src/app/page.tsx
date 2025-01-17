27899e15622e30fa253a417ac4cff5a753ed205b
Nishanth <nishanthrn1@gmail.com>
2025-01-17 23:32:32 +0530

    fix:made changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,5 +2,9 @@ import Events from "@/components/Events/Events";
 import Hero from "@/components/Hero/Hero";
 
 export default function page() {
-  return <></>;
+  return (
+    <>
+      <Hero />
+    </>
+  );
 }

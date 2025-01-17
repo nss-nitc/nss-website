c90776e7858834088d0d8b77f46908252d370db3
MOHAMEDADHIL678 <mohamedadhil678@gmail.com>
2025-01-18 01:32:50 +0530

    navbar added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,7 @@
-import Events from "@/components/Events/Events";
-import Navbar from "@/components/Navbar/Navbar";
 
 export default function page() {
   return (
     <>
-    <Navbar/>
     </>
   );
 }

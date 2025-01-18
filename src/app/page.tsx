9a02cbdb99a431bac79e9a08bbb1c5e20ad8c4fa
MOHAMEDADHIL678 <mohamedadhil678@gmail.com>
2025-01-18 14:28:20 +0530

    Navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
+import Navbar from "@/components/Navbar/Navbar";
 
 export default function page() {
   return (
     <>
+    <Navbar/>
     </>
   );
 }
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -2,7 +2,7 @@
 import Link from "next/link";
 import React, { useState } from "react";
 
-const Navbar = () => {
+const Nav = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   const toggleMenu = () => {
@@ -10,15 +10,15 @@ const Navbar = () => {
   };
 
   return (
-    <header className="flex flex-col md:flex-row w-full shadow-md">
+    <header className="absolute top-0 left-0 w-full flex flex-col md:flex-row shadow-md bg-[#FEFBEA] z-50">
       {/* Left Section */}
-      <div className="flex items-center bg-[#1F293E] text-white px-5 md:w-1/3">
+      <div className="flex items-center bg-[#202A44] text-white px-5 py-4 md:w-1/3">
         <img
-          className="w-[90px] h-[83px] rounded-full mr-6"
+          className="w-14 h-15 rounded-full mr-6 -my-2"
           src="/images/NSS-LOGO.png"
           alt="Logo"
         />
-        <div className="uppercase text-sm font-bold Momcake tracking-wider">
+        <div className="uppercase text-sm font-bold tracking-wider">
           NSS NIT Calicut
         </div>
         <div
@@ -35,48 +35,44 @@ const Navbar = () => {
       <div
         className={`${
           isMenuOpen ? "flex" : "hidden"
-        } flex-col md:flex md:flex-row md:w-2/3 bg-[#F9F3E6] items-center px-5 py-4`}
+        } flex-col md:flex md:flex-row md:w-2/3 items-center px-5 py-4`}
       >
-        <nav
-          className={`flex flex-col md:flex-row w-full md:w-auto items-center justify-end gap-5 Poppins ml-auto ${
-            isMenuOpen ? "block" : "hidden"
-          } md:flex`}
-        >
+        <nav className="flex flex-col md:flex-row w-full md:w-auto items-center justify-end gap-5 ml-auto">
           <Link
             href="/about"
-            className="text-[#001f3f] text-base  hover:text-[#ffa500] transition duration-300"
+            className="text-[#001f3f] text-base font-bold hover:text-[#ffa500] transition duration-300"
           >
             About
           </Link>
           <Link
             href="/events"
-            className="text-[#001f3f] text-base  hover:text-[#ffa500] transition duration-300"
+            className="text-[#001f3f] text-base font-bold hover:text-[#ffa500] transition duration-300"
           >
             Events
           </Link>
           <Link
             href="/team"
-            className="text-[#001f3f] text-base  hover:text-[#ffa500] transition duration-300"
+            className="text-[#001f3f] text-base font-bold hover:text-[#ffa500] transition duration-300"
           >
             Team
           </Link>
           <Link
             href="/gallery"
-            className="text-[#001f3f] text-base  hover:text-[#ffa500] transition duration-300"
+            className="text-[#001f3f] text-base font-bold hover:text-[#ffa500] transition duration-300"
           >
             Gallery
           </Link>
           <Link
             href="/contact"
-            className="text-[#001f3f] text-base  hover:text-[#ffa500] transition duration-300"
+            className="text-[#001f3f] text-base font-bold hover:text-[#ffa500] transition duration-300"
           >
             Contact Us
           </Link>
         </nav>
-        <button className="mt-4 md:mt-0 md:ml-5 Poppins">
+        <button className="mt-4 md:mt-0 md:ml-5">
           <Link
             href="/login"
-            className="bg-[#001f3f] text-white py-2 px-6 rounded-full text-base  hover:bg-[#000d1a] transition duration-300"
+            className="bg-[#001f3f] text-white py-2 px-6 rounded-full text-base font-bold hover:bg-[#000d1a] transition duration-300"
           >
             Login
           </Link>
@@ -86,4 +82,4 @@ const Navbar = () => {
   );
 };
 
-export default Navbar;
+export default Nav;

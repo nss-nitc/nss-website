1f150fb99243882b10abcdc4617fbd01314c1c55
ARUNKRISH11 <krish.tech.arun.11@gmail.com>
2025-01-31 22:43:10 +0530

    event added padding

--- src/components/Events/Events.tsx
+++ src/components/Events/Events.tsx
@@ -12,16 +12,18 @@ export default function Events() {
       {events.map((event, index) => {
         if (index % 2 === 0) {
           return (
-            <div className="flex flex-col md:flex-row justify-between ">
-              <div className="flex flex-col w-screen md:w-[55%] justify-center px-8 md:ps-14 py-8 md:my-5 gap-2 bg-[#57A2D8] bg-opacity-[54%] rounded-t-xl md:rounded-e-xl">
-                <h1 className="Poppins font-bold text-[25px] md:text-[30px] ">{event.name}</h1>
+            <div className="flex flex-col md:flex-row py-5 justify-between ">
+              <div className="flex flex-col md:mx-0 mx-10 rounded-t-3xl md:rounded-l-none w-fit md:w-[55%] justify-center px-8 md:ps-14 py-8 md:my-5 gap-2 bg-[#57A2D8] bg-opacity-[54%] md:rounded-e-3xl">
+                <h1 className="Poppins font-bold text-[25px] md:text-[30px] ">
+                  {event.name}
+                </h1>
                 <p className="Poppins text-[20px] lg:text-[26px] ">
                   {event.paragraph}
                 </p>
               </div>
-              <div className="flex w-screen md:w-[40%] justify-center items-center mb-6 md:my-5 ">
+              <div className="flex w-screen md:w-[40%] px-10 sm:px-0 justify-center items-center mb-6 md:my-5 ">
                 <img
-                  className="w-full h-fit rounded-t-none rounded-b-xl md:rounded-s-xl"
+                  className="w-full h-fit md:mx-0 mx-10 rounded-b-3xl md:rounded-r-none md:rounded-s-3xl"
                   src={event.imageUrl}
                   alt={event.imageAlt}
                 />
@@ -33,20 +35,22 @@ export default function Events() {
             <div className="flex flex-col md:flex-row justify-between">
               <div className="hidden w-[40%] justify-center items-center md:my-5 md:flex">
                 <img
-                  className=" w-full h-fit rounded-e-xl"
+                  className=" w-full h-fit md:mx-0 mx-10 md:rounded-e-3xl"
                   src={event.imageUrl}
                   alt={event.imageAlt}
                 />
               </div>
-              <div className="flex flex-col w-screen md:w-[55%] justify-center px-8 md:ps-14 py-8 md:my-5 gap-2 bg-[#57A2D8] bg-opacity-[54%] rounded-t-xl md:rounded-s-xl">
-                <h1 className="Poppins font-bold text-[26px] md:text-[30px] ">{event.name}</h1>
+              <div className="flex flex-col w-fit md:mx-0 mx-10 rounded-t-3xl md:rounded-r-none md:w-[55%] justify-center px-8 md:ps-14 py-8 md:my-5 gap-2 bg-[#57A2D8] bg-opacity-[54%] md:rounded-s-3xl ">
+                <h1 className="Poppins font-bold text-[26px] md:text-[30px] ">
+                  {event.name}
+                </h1>
                 <p className="Poppins text-[20px] lg:text-[26px] ">
                   {event.paragraph}
                 </p>
               </div>
-              <div className="flex w-screen justify-center items-center mb-6 md:hidden">
+              <div className="flex w-screen px-10 sm:px-0 justify-center items-center mb-6 md:hidden">
                 <img
-                  className=" w-full h-fit rounded-b-xl"
+                  className=" w-full h-fit md:mx-0 mx-10 rounded-b-3xl"
                   src={event.imageUrl}
                   alt={event.imageAlt}
                 />

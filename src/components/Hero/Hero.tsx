937b9d1aa1f403f5271c2984d40644644ec2fea6
1-Nishanth-1 <nishanthrn1@gmail.com>
2025-01-31 23:00:18 +0530

    fix: fixed hero

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -24,13 +24,13 @@ export default function Hero() {
           </h1>
         </div>
       </div>
-      <div className="flex h-[320px] font-mom">
+      <div className="flex h-[200px] md:h-[320px] font-mom">
         <div className="w-1/3 relative group overflow-hidden">
           <img
             src="images/LetsTeachLink.svg"
             className="h-full w-full object-cover group-hover:scale-110 transition-transform duration-500"
           />
-          <div className="absolute bottom-10 left-5 text-3xl sm:text-4xl font-semibold text-white z-10">
+          <div className="absolute bottom-3 md:bottom-10 left-5 text-2xl sm:text-4xl font-semibold text-white z-10">
             <img src="images/book Icon.svg" className="my-7"></img>
             Lets Teach
           </div>
@@ -43,7 +43,7 @@ export default function Hero() {
             className="h-full w-full object-cover group-hover:scale-110 transition-transform duration-500"
           />
           <div className="absolute inset-0 bg-[#E0202B] bg-opacity-30"></div>
-          <div className="absolute bottom-10 left-5 text-3xl sm:text-4xl font-semibold text-white z-10">
+          <div className="absolute bottom-3 md:bottom-10 left-5 text-2xl sm:text-4xl font-semibold text-white z-10">
             <img src="images/blood icon.svg" className="my-7"></img>
             Blood Forum
           </div>
@@ -54,14 +54,14 @@ export default function Hero() {
             className="h-full w-full object-cover group-hover:scale-110 transition-transform duration-500"
           />
           <div className="absolute inset-0 bg-black bg-opacity-30"></div>
-          <div className="absolute bottom-10 left-5 text-3xl sm:text-4xl font-semibold text-white z-10">
+          <div className="absolute bottom-3 md:bottom-10 left-5 text-2xl sm:text-4xl font-semibold text-white z-10">
             our events
           </div>
         </div>
       </div>
 
       <div className="flex flex-col items-center justify-center w-full font-poppins">
-        <div className="w-[85vw] bg-[#1F293E] text-white px-8 sm:px-16 py-8 rounded-3xl my-10">
+        <div className="w-[75vw] bg-[#1F293E] text-white px-8 sm:px-16 py-8 rounded-3xl my-10">
           <h1 className="text-center md:text-5xl text-3xl font-semibold mb-5">
             About Us
           </h1>
@@ -72,7 +72,7 @@ export default function Hero() {
             most.
           </p>
         </div>
-        <div className=" w-[85vw]  bg-[#C0C9DB] text-black px-8 sm:px-16 py-8 rounded-3xl ">
+        <div className=" w-[75vw]  bg-[#C0C9DB] text-black px-8 sm:px-16 py-8 rounded-3xl ">
           <h1 className="text-center md:text-5xl text-3xl font-semibold mb-5">
             What we do
           </h1>
